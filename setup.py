dc2d31768e4957cb789961aecedf6aa210e0b5f7
mey0153 <sreeikarath@gmail.com>
2022-09-14 08:21:29 +1200

    Initial project commit and project folders added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this dataset is to predict sentiment score for news headline.',
+    author='SD',
+    license='',
+)
